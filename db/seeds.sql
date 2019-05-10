48802f43eb89cf90f5f49edc6a5b5a965ba46a8f
Pavel Zapolski <pavelzapolski@Pavels-MBP.fios-router.home>
2019-05-10 00:14:25 -0400

    first push

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("CHICKEN with bacon", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("beef", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("pork with onions", true);
